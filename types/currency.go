5ebe471d0f4c490db42c79ba8d534483b1a55043
soonkuk <soonkuk@gmail.com>
2023-12-03 12:16:18 +0900

    feat: add Regex to check special characters

--- types/currency.go
+++ types/currency.go
@@ -10,6 +10,7 @@ var (
 	MinLengthCurrencyID = 3
 	MaxLengthCurrencyID = 10
 	ReValidCurrencyID   = regexp.MustCompile(`^[A-Z0-9][A-Z0-9_\.\!\$\*\@]*[A-Z0-9]$`)
+	ReSpcecialChar      = regexp.MustCompile(`^[^\s:/?#\[\]@]*$`)
 )
 
 type CurrencyID string
